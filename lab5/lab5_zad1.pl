7d7a9fbe48bbbad9fa40c1d7ed308ed05ac419b2
Przemysław Rogalski <p.rogalski@o2.pl>
2018-11-29 20:18:08 +0100

    LAB-5 excercise 1 and 2

---
+++ lab5/lab5_zad1.pl
@@ -0,0 +1,5 @@
+didBetter(X,Y,order(X,Y,_)).
+didBetter(X,Y,order(X,_,Y)).
+didBetter(X,Y,order(_,X,Y)).
+clue1(S):-didBetter(child(peter,_),child(_,red),S).
+clue2(S):-didBetter(child(jack,gold),child(_,green),S).
---
+++ lab5/lab5_zad2.pl
@@ -0,0 +1,29 @@
+onStreet(X,s(X,_,_,_,_)).
+onStreet(X,s(_,X,_,_,_)).
+onStreet(X,s(_,_,X,_,_)).
+onStreet(X,s(_,_,_,X,_)).
+onStreet(X,s(_,_,_,_,X)).
+middle(X,s(_,_,X,_,_)).
+first(X,s(X,_,_,_,_)).
+left(X,Y,s(X,Y,_,_,_)).
+left(X,Y,s(_,X,Y,_,_)).
+left(X,Y,s(_,_,X,Y,_)).
+left(X,Y,s(_,_,_,X,Y)).
+right(X,Y,S):-left(Y,X,S).
+next_to(X, Y, S) :- left(X, Y, S). 
+next_to(X, Y, S) :- left(Y, X, S).
+clue(S):-onStreet([anglik,czerwony,_,_,_],S),
+    onStreet([hiszpan,_,_,_,pies],S),
+    onStreet([_,zielony,_,kawa,_],S),
+    onStreet([ukrainiec,_,_,herbata,_],S),
+    right([_,zielony,_,_,_],[_,ivory,_,_,_],S),
+    onStreet([_,_,tenis,_,waz],S),
+    onStreet([_,żółty,szachy,_,_],S),
+    middle([_,_,_,mleko,_],S),
+    first([norweg,_,_,_,_],S),
+    next_to([_,_,rugby,_,_],[_,_,_,_,lis],S),
+    next_to([_,_,szachy,_,_],[_,_,_,_,koń],S),
+    onStreet([_,_,siatkówka,sok,_],S),
+    onStreet([japończyk,_,go,_,_],S),
+    next_to([norweg,_,_,_,_],[_,niebieski,_,_,_],S),
+    next_to([_,_,_,herbata,_],[_,_,_,kakao,_],S).
