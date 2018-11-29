c5b4f362927bc56adf9393cab531a614a3cafe8a
Przemysław Rogalski <p.rogalski@o2.pl>
2018-11-29 20:40:38 +0100

    LAB-6 excercise 1 and 2

---
+++ lab6/lab6_zad1.pl
@@ -0,0 +1,13 @@
+edge(a,b).
+edge(b,c).
+edge(a,d).
+edge(d,e).
+edge(d,f).
+edge(f,a).
+edge(f,g).
+go(X,X,_).
+go(X,Y,T):-edge(X,Z),not(member(Z,[X|T])),go(Z,Y,[X|T]).
+go1(X,X,T,[X|T]).
+go1(X,Y,T,P):-edge(X,Z),not(member(Z,[X|T])),go1(Z,Y,[X|T],P).
+go2(X,X,_,[X]).
+go2(X,Y,T,[X|P]):-edge(X,Z),not(member(Z,[X|T])),go2(Z,Y,[X|T],P).
